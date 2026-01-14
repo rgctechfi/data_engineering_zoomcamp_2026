f3a25604d1bea5215edb83a32f800fcbddcdd2d5
rgctechfi <rgctechfipro@outlook.com>
2026-01-14 15:46:18 +0100

    docker + pipeline

---
+++ Module_1_Docker/pipeline/pipeline.py
@@ -0,0 +1,10 @@
+# %%
+'''
+Command: 'c:/Users/RuruA/Documents/MEGASyncPC/Administratifs/LEARNING/IT/Data_Engineering_Zoomcamp_2026/data_engineering_zoomcamp_2026/.venv/Scripts/python.exe -m pip install ipykernel -U --force-reinstall
+'''
+import sys
+print("arguments", sys.argv)
+
+day = int(sys.argv[1])
+print(f"Running pipeline for day {day}")
+# %%
