7814ea048d239dc9e15284c9cf9bbed3f775da97
rgctechfi <rgctechfipro@outlook.com>
2026-01-16 17:51:15 +0100

    add dockerfile configuration

--- Module_1_Docker/pipeline/test.py
+++ Module_1_Docker/pipeline/test.py
@@ -1,3 +1,14 @@
+#if crash freeze for interactive python
+'''
+Command: uv pip install ipykernel -U --force-reinstall
+import os
+os._exit(00)
+
+from ipylab import JupyterFrontEnd
+app = JupyterFrontEnd()
+app.commands.execute('kernelmenu:restart')
+
+'''
 # Import dependancies
 
 import sys
