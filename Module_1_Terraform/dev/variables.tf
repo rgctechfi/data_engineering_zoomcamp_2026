7d9b9ba9ea8073b9ad53d35caa632150a735e5e6
rgctechfi <rgctechfipro@outlook.com>
2026-01-25 14:27:47 +0100

    terraform infra finished

--- Module_1_Terraform/dev/variables.tf
+++ Module_1_Terraform/dev/variables.tf
@@ -16,8 +16,8 @@ variable "location" {
 }
 
 variable "bq_dataset_name" {
-  description = "My BigQuery Dataset Name"
-  default     = "demo_dataset"
+  description = "My Big Query dataset"
+  default     = "ny_taxi_bigquery_dataset"
 }
 
 variable "gcs_bucket_name" {
